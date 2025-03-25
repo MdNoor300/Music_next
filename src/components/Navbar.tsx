9150d3af1d0e2d9dfb1502b9aea769c6b6704811
MdNoor300 <sehzad.noor98@gmail.com>
2025-03-25 15:14:16 +0600

    navbar section modifying

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,16 +14,29 @@ function Navbar({ className }: { className?: string }) {
     >
       <Menu setActive={setActive}>
         <Link href={"/"}>
-          <MenuItem setActive={setActive} active={active} item="Home">
-          Noor
-          </MenuItem>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
         </Link>
         <MenuItem setActive={setActive} active={active} item="Our Courses">
-        <div className="flex flex-col space-y-4 text-sm">
-        <HoveredLink href="/courses">All Courses</HoveredLink>
-        </div>
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+          </div>
         </MenuItem>
-<ModeToggle />
+        <Link href={"/contuct"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contuct Us"
+          ></MenuItem>
+        </Link>
+        <ModeToggle />
       </Menu>
     </div>
   );
