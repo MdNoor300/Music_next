f8a84b2764123b07d44d5ff7d2835bd70ac3035b
MdNoor300 <sehzad.noor98@gmail.com>
2025-04-10 15:30:38 +0600

    card responsive and all features done

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,21 +1,24 @@
 "use client";
-import courseData from "../data/music_courses.json";
 import Link from "next/link";
+import courseData from "../data/music_courses.json";
 import { BackgroundGradient } from "./ui/background-gradient";
 
 interface Course {
-        id: number,
-        title: string,
-        slug: string,
-        description: string,
-        price: number,
-        instructor: string,
-        isFeatured: boolean,
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
 }
-const FeaturedCourses = () => {
-    const featuredCourses = courseData.courses.filter((course:Course) => course.isFeatured)
-  
-    return (
+
+function FeaturedCourses() {
+  const featuredCourses = courseData.courses.filter(
+    (course: Course) => course.isFeatured
+  );
+
+  return (
     <div className="py-12 bg-gray-900">
       <div>
         <div className="text-center">
@@ -27,31 +30,35 @@ const FeaturedCourses = () => {
           </p>
         </div>
       </div>
-      <div className="mt-10 lg:px-40 md:px-20 sm:px-10 px-5">
-        <div className="lg:px-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-            {featuredCourses.map((course:Course) => (
-                <div key={course.id} className="flex justify-center">
-                  <BackgroundGradient>
-                    <div className="p-6">
-                      <p className="font-bold text-black">{course.title}</p>
-                      <p>{course.description}</p>
-                      <p className="font-bold text-black">{course.instructor}</p>
-                    </div>
-                  </BackgroundGradient>
+      <div className="mt-10 mx-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+          {featuredCourses.map((course: Course) => (
+            <div key={course.id} className="flex justify-center">
+              <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                    {course.title}
+                  </p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                    {course.description}
+                  </p>
+                  <Link href={`/courses/${course.slug}`}>Learn More</Link>
                 </div>
-            ) )}
+              </BackgroundGradient>
+            </div>
+          ))}
         </div>
       </div>
       <div className="mt-20 text-center">
         <Link
           href={"/courses"}
-          className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-teal-600 border border-transparent rounded-md shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+          className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
         >
-          View All Courses
+          View All courses
         </Link>
       </div>
     </div>
   );
-};
+}
 
 export default FeaturedCourses;
