08cee1e8f9732262656ca837654c119be9efaa29
MdNoor300 <sehzad.noor98@gmail.com>
2025-04-11 16:57:40 +0600

    instructor section all featurs done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChoseUs from "@/components/WhyChoseUs";
@@ -12,6 +13,7 @@ export default function Home() {
               <WhyChoseUs/>
               <TestimonialCards/>
               <UpcomingWebinars/>
+              <Instructors/>
     </main>
   );
 }
