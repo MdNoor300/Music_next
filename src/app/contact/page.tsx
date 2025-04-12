7a838968d0f8ed1c862ad71c9e4efa37c9b8f0d0
MdNoor300 <sehzad.noor98@gmail.com>
2025-04-12 17:58:39 +0600

    did some changes to add meteor effect in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+"use client";
+import React from "react";
+import { Meteors } from "@/components/ui/meteors";
 function page() {
   return (
-    <div>page333</div>
-  )
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
+      
+      <Meteors number={40} className="h-1 w-0.5 fixed inset-0 z-0" />
+    </div>
+  );
 }
 
-export default page
+export default page;
